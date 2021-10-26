b513082e936d116636e61d623da5ed68d426bfb6
Vova <vzakharov@gmail.com>
2021-10-26 10:26:02 +0300

    to implement ip limiting

--- pages/widget/examples.vue
+++ pages/widget/examples.vue
@@ -1,42 +1,47 @@
 <template>
-  <div>
-    <b-container class="p-3" style="max-width: 960px">
-      <b-row>
-        <b-col cols="4" style="max-width:300px" class="p-2 mx-4">
-          <b-list-group>
-            <b-list-group-item button v-for="w in widgets" :key="w._id" :active="w==widget" @click="setWidget(w)" v-text="w.display.name"/>
-          </b-list-group>
-        </b-col>
-        <b-col cols="7" class="bg-white p-2">
-          <div v-if="widget">
-            <!-- <h3 v-text="widget.display.name"/>
-            <small>Here’s how your widget might look like (shadow not included):</small> -->
-            <WidgetProper ref="widget" v-bind="{widget}" :key="widget._id" class="shadow mt-2 mb-4"/>
-            <LabeledInput
-              caption="Embed link"
-              :value="iframeCode"
-              :multiline="true"
-              :props="{
-                style: 'font-family: monospace',
-                disabled: true,
-                rows: 1
-              }"
-            />
-            <b-button :variant="copied ? 'light' : 'outline-secondary'" size="sm" v-text="!copied ? 'Copy' : 'Copied!'"
-              @click="$copyText(iframeCode).then(() => copied=true)"
-            />
-            <LabeledInput
-              caption="Use with your own API key"
-              v-model="widget.apiKey"
-              :props="{
-                placeholder: 'sk-...'
-              }"
-            />
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+  <b-container fluid="md" class="mt-3">
+    <b-row>
+      <b-col cols="3" class="d-none d-sm-block">
+        <b-list-group>
+          <b-list-group-item button v-for="w in widgets" :key="w._id" :active="w==widget" @click="setWidget(w)">
+            {{ w.display.name }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+      <b-col xs="12" sm="9" class="bg-white">
+        <b-dropdown class="d-sm-none" variant="outline-primary" :text="widget ? widget.display.name : 'Pick a widget'">
+          <b-dropdown-item v-for="w in widgets" :key="w._id" :active="w==widget" @click="setWidget(w)">
+            {{ w.display.name }}
+          </b-dropdown-item>
+        </b-dropdown>
+        <div v-if="widget">
+          <!-- <h3 v-text="widget.display.name"/>
+          <small>Here’s how your widget might look like (shadow not included):</small> -->
+          <WidgetProper ref="widget" v-bind="{widget}" :key="widget.id" class="shadow mt-2 mb-4"/>
+          <LabeledInput
+            caption="Embed link"
+            :value="iframeCode"
+            :multiline="true"
+            :props="{
+              style: 'font-family: monospace',
+              disabled: true,
+              rows: 1
+            }"
+          />
+          <b-button :variant="copied ? 'light' : 'outline-secondary'" size="sm" v-text="!copied ? 'Copy' : 'Copied!'"
+            @click="$copyText(iframeCode).then(() => copied=true)"
+          />
+          <LabeledInput
+            caption="Use with your own API key"
+            v-model="widget.apiKey"
+            :props="{
+              placeholder: 'sk-...'
+            }"
+          />
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
