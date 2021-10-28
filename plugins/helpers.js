e10d3ad914cf51bbccad0a04ec9b12636631b970
Vova <vzakharov@gmail.com>
2021-10-28 18:26:42 +0300

    ...

--- plugins/helpers.js
+++ plugins/helpers.js
@@ -1,8 +1,8 @@
 import { filter, isObject, pick, get, keys } from 'lodash'
 // import Bubble from '@/plugins/bubble'
 
-function canRunWidget({code, godMode, apiKey} = this) {
-  if ( godMode || apiKey )
+function canRunWidget({code, godMode, apiKey, widget} = this) {
+  if ( godMode || apiKey || widget.id=='demo')
     return true
   else
     return code && code.runsLeft > 0
