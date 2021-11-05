0561a79ebff4cc23f2065bb4d60d1fe686cb5fab
Vova <vzakharov@gmail.com>
2021-11-05 18:01:35 +0300

    basic stats component

--- components/Widget/Stats.vue
+++ components/Widget/Stats.vue
@@ -1,25 +1,57 @@
 <template>
   <div>
-    Hello world
+    <b-table
+      :items="actionCounts"
+    >
+    </b-table>
   </div>
 </template>
 
 <script>
 
-  import { groupBy } from 'lodash'
+  import { chain, uniqBy, map } from 'lodash'
 
   export default {
-
 
     props: ['value'],
 
     data() {
       return {
-        ...value
+        ...this.value
       }
     },
 
     computed: {
+
+      actionCountsByUser() {
+        return chain(this.events)
+          .groupBy('user')
+          .mapValues(theirEvents => chain(theirEvents)
+            .countBy('action').value()).value()
+      },
+
+      actionCounts() {
+        let actionCounts = chain(this.events)
+          .groupBy('action')
+          .mapValues(actions => 
+            uniqBy(actions, 'user')
+            .length
+          )
+          .map((count, action) => 
+            ({action, count})
+          ).value()
+        
+        const conversionPercentage = ( item, i, j ) =>
+          i ? `${Math.round(100*item.count/actionCounts[j].count)}%` : undefined
+
+        actionCounts = map(actionCounts, ( item, i ) => ({
+          ...item, 
+          stepConversion: conversionPercentage(item, i, i-1),
+          totalConversion: conversionPercentage(item, i, 0)
+        }))
+
+        return actionCounts
+      }
 
     }
 
---
+++ pages/test/stats.vue
@@ -0,0 +1,31 @@
+<template>
+  <WidgetStats v-model="stats"/>
+</template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+        stats: {
+          events: [
+            {user: 1, action: 'open'}, 
+            {user:1, action: 'run'}, 
+            {user:2, action: 'open'}, 
+            {user:1, action:'open'},
+            {user:2, action: 'run'},
+            {user:2, action: 'cta'},
+            {user:3, action: 'open'}
+          ]
+        }
+      }
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
