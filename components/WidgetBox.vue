aa004144c2e101df52bd910dab8094feeb653916
Vova <vzakharov@gmail.com>
2021-11-24 15:57:07 +0300

    margin fix

--- components/WidgetBox.vue
+++ components/WidgetBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-row align-h="center" align-v="center" class="my-5 p-0">
+  <b-row align-h="center" align-v="center" class="m-0 my-5 p-0">
     <b-col align-self="center" cols="11" class="bg-light border shadow" style="max-width:600px">
       <slot/>
     </b-col>
