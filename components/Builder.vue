d2f029e175d8a830d47d080ad4d37d8d7b2ac95c
Vova <vzakharov@gmail.com>
2021-12-05 11:26:40 +0300

    builder

---
+++ components/Builder.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container class="mt-5">
+    <b-row>
+      <b-col>
+        <b-textarea v-model="code"/>
+      </b-col>
+      <b-col>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+
+  export default {
+
+    props: ['args'],
+
+    data() {
+      return this.args
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/builder/_build.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Builder :args="$data"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    data() {
+
+      return {
+        code: "helloworld"
+      }
+
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
