6e25b7b335fdc3a1453b37e1438e0f8c2bbe6be6
Vova <vzakharov@gmail.com>
2021-12-15 17:47:30 +0300

    lifetime premium (redirect to twitter post for now)

---
+++ pages/lifetime-premium.vue
@@ -0,0 +1,11 @@
+<script>
+
+  export default {
+
+    async middleware({ redirect, route: { params: { tail }} }) {
+      redirect('https://twitter.com/vovahimself/status/1471124823703228421?s=20')
+    },
+
+  }
+
+</script>
