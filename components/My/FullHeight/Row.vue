9faa1082f059cffce89f001647d7d33472a05c33
Vova <vzakharov@gmail.com>
2022-01-24 12:58:54 +0300

    fullheight (to remove)

---
+++ components/My/FullHeight/Row.vue
@@ -0,0 +1,11 @@
+<template>
+  <MyFullHeight tag="b-row" v-bind="$props">
+    <slot/>
+  </MyFullHeight>
+</template>
+
+<script>
+export default {
+  props: ['align-v']
+}
+</script>
---
+++ components/My/FullHeight/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :is="tag" v-bind="$props"
+    :style="`height: calc(100vh - ${top}px)`"
+  >
+    <slot/>
+  </component>
+</template>
+
+<script>
+  export default {
+    props: ['tag', 'align-v'],
+    
+    data() {
+      return {
+        top: 0
+      }
+    },
+
+    mounted() {
+      this.top = this.$el.offsetTop
+    }
+
+  }
+</script>
