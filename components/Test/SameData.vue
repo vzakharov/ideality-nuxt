be3690f00b42af8e693cfc0bed9fe1ea57bfdf7e
Vova <vzakharov@gmail.com>
2022-01-25 19:55:26 +0300

    test same data (successful)

---
+++ components/Test/SameData.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-input v-model="message"/>
+</template>
+
+<script>
+
+  let data = {
+    message: 'Hello world'
+  }
+
+  export default {
+
+    data() {
+      return data
+    }
+
+  }
+
+</script>
---
+++ pages/test/samedata.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TestSameData/>
+    <TestSameData/>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
