23d629d9848f9c3d36b49810167bb9c4b208b0fa
Vova <vzakharov@gmail.com>
2022-01-25 18:57:58 +0300

    MyNavButton

---
+++ components/My/Nav/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-button
+    v-bind="{
+      size: 'sm',
+      variant: 'outline-secondary',
+      ...item
+    }"
+    @click.prevent="item.onclick && item.onclick()"
+    onclick="this.blur()"
+  >
+    <b-icon :icon="item.icon"/>
+  </b-button>
+</template>
+
+<script>
+
+  export default {
+    props: ['item']
+  }
+
+</script>
+
+<style>
+
+</style>
--- components/Nav/Build/View.vue
+++ components/Nav/Build/View.vue
@@ -1,18 +1,14 @@
 <template>
   <b-row class="border-bottom bg-white py-1">
     <b-col>
-      <b-button v-for="item in [
+      <MyNavButton v-for="item in [
           { icon: 'file-earmark', to: { name: 'i-new' }, variant: 'outline-primary' },
-          { icon: 'folder2-open', onclick() { model.expanded = !model.expanded }, variant: model.expanded && 'secondary' },
+          { icon: model.expanded ? 'folder2-open' : 'folder', onclick() { model.expanded = !model.expanded } },
           { icon: 'link-45deg', to: { name: 'i-slug', params: build } },
         ]" 
         :key="item.icon"
-        size="sm" :variant="item.variant || 'outline-secondary'" 
-        :to="item.to"
-        @click.prevent="item.onclick && item.onclick()"
-      >
-        <b-icon :icon="item.icon"/>
-      </b-button>              
+        v-bind="{item}"
+      />
       <b-button-close class="float-right">
         <nuxt-link :to="{name:'ideas'}" class="nocolor">
           ×
