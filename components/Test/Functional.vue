808b8e4c740db767f626b27059242a6a562fbce7
Vova <vzakharov@gmail.com>
2022-01-26 18:24:41 +0300

    tested: nested functional components do not work

---
+++ components/Test/Functional.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <div>
+    {{ JSON.stringify(props) }}
+    <TestFunctional v-if="!props.stop" v-bind="{...props, stop: true}"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    props: ['close', 'items', 'stop']
+
+  }
+
+</script>
---
+++ pages/test/functional.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <!-- <MyNavButton v-for="item in [
+      { icon: 'chevron-double-left', to: { name: 'index'} },
+      { icon: 'file-earmark', to: { name: 'i-new' }, variant: 'outline-primary' }
+    ]" 
+        :key="item.icon"
+        v-bind="item"
+      /> -->
+  <TestFunctional v-if="mounted" :items="[
+      { icon: 'chevron-double-left', to: { name: 'index'} },
+      { icon: 'file-earmark', to: { name: 'i-new' }, variant: 'outline-primary' }
+    ]" 
+  />
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+<style>
+
+</style>
