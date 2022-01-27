548feb8f2dbb6d1fba601b4d8d8b641aa54ca241
Vova <vzakharov@gmail.com>
2022-01-27 12:13:47 +0300

    testing contenteditable

---
+++ pages/test/editable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="p-3">
+    <span class="p-1"
+      v-for="item, i in items" :key="i" 
+      v-html="item.content" contenteditable 
+      @input="currentContent = $event.target.innerHTML"
+      @blur="$set(item, 'content', $event.target.innerHTML)"
+    />
+    <b-icon icon="plus-circle"
+      @click="items = [...items, {}]"
+    />
+    <p class="mt-2" v-text="items.map(i=>i.content || '').join()"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+        currentContent: '',
+        items: []
+      }
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
