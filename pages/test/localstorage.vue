05975e783551616605f639ffcc9265aa0797c4ff
Vova <vzakharov@gmail.com>
2022-02-12 03:17:31 +0300

    localstorage getter/setter page

---
+++ pages/test/localstorage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="p-3">
+    <template v-if="hashRoute">
+      Local storage content for key <strong v-text="hashRoute"/>:
+      <CodeInput v-model="content"/>
+    </template>
+    <Loading v-else message="Please wait..."/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    computed: {
+      content: {
+        get() {
+          return localStorage.getItem(this.hashRoute)
+        },
+
+        set(value) {
+          localStorage.setItem(this.hashRoute, value)
+        }
+      }
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
