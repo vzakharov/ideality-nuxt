6a143f918471b5e3d2f8067e9de83910e1fec89c
Vova <vzakharov@gmail.com>
2022-02-22 17:13:09 +0300

    Create yaml.vue

---
+++ pages/test/yaml.vue
@@ -0,0 +1,32 @@
+<template>
+  <CodeInput :value="dump(object)"/>
+</template>
+
+<script>
+
+  import { dump } from 'js-yaml'
+
+  export default {
+
+    computed: {
+      object() {
+        let a = { b:1 }
+        return {
+          a,
+          hello: 'world',
+          array: [a,2,3]
+        }
+      }
+    },
+
+    methods: {
+      dump
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
