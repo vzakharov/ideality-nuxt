2eb39828d23f440f5c1fc6a9ef9af200d41db8d9
Vova <vzakharov@gmail.com>
2022-02-22 17:58:58 +0300

    slugKey→display

--- components/My/Dropdown.vue
+++ components/My/Dropdown.vue
@@ -1,17 +1,17 @@
 <template>
     <b-dropdown :variant="value ? 'outline-dark' : 'outline-secondary'" 
-      :text="upperFirst(maybe(value)[slugKey] || value) || placeholder || 'Please choose'"
+      :text="upperFirst(maybe(value)[display] || value) || placeholder || 'Please choose'"
     >
       <b-dropdown-item
         :active="!value"
         @click="$emit('input')"
       ><em>None</em></b-dropdown-item>
       <b-dropdown-item 
-        v-for="choice in choices" :key="choice[slugKey] || choice" 
-        :active="slugKey ? choice[slugKey]==maybe(value)[slugKey] : choice==value"
+        v-for="choice in choices" :key="choice[display] || choice" 
+        :active="display ? choice[display]==maybe(value)[display] : choice==value"
         @click="$emit('input', choice)"
       >
-        {{ upperFirst(choice[slugKey] || choice) }}
+        {{ upperFirst(choice[display] || choice) }}
       </b-dropdown-item>
     </b-dropdown>
 </template>
@@ -22,7 +22,7 @@ import { upperFirst } from 'lodash'
 
 export default {
 
-  props: ['value', 'choices', 'placeholder', 'slugKey'],
+  props: ['value', 'choices', 'placeholder', 'display'],
 
   methods: { upperFirst }
 
