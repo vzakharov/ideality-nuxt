67ae3863608a46a66af5e930af2eebcd8395af0a
Vova <vzakharov@gmail.com>
2022-02-23 09:42:53 +0300

    Create editable-2.vue

---
+++ pages/test/editable-2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div contenteditable
+      @input="log($event)"
+    >
+      <span v-for="item, i in items" :key=i v-text="item"
+        @click="index=i"
+        :class="{ gray: i!=index }"
+      />
+    </div>
+    {{ items }}
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    data() {
+      return {
+        items: [
+          'Hello',
+          ' world',
+          '!'
+        ],
+        index: null
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
